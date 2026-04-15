deb2ccb70d721ea1ec82891055f86611f0e25dc5
olsaa0 <166027576+olsaa0@users.noreply.github.com>
2026-04-15 21:13:22 +0200

    Add API key verification function

---
+++ PersonalProject/auth/security.py
@@ -0,0 +1,7 @@
+from fastapi import Header, HTTPException
+
+API_KEY = "my_super_secret_crypto_key"
+
+def verify_api_key(x_api_key: str = Header(...)):
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=403, detail="Unauthorized Access!")
