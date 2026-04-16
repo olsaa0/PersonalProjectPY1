642cf73a842a3414b0ed787aa5d3b5bcd5f3bc81
olsaa0 <166027576+olsaa0@users.noreply.github.com>
2026-04-16 15:26:21 +0200

    Fix formatting and add missing newline in security.py

--- PersonalProject/auth/security.py
+++ PersonalProject/auth/security.py
@@ -5,3 +5,4 @@ API_KEY = "my_super_secret_crypto_key"
 def verify_api_key(x_api_key: str = Header(...)):
     if x_api_key != API_KEY:
         raise HTTPException(status_code=403, detail="Unauthorized Access!")
+.
